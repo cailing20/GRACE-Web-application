8c370fd9ee5a5e98ea5f193faf7a643d2ea5ced8
Yi <yi.du@utsouthwestern.edu>
2016-08-04 10:53:34 -0500

    remove test_php

--- test_php/test_arraySearch.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo array_search(5,[1,2,5,6]);
-?>
--- test_php/test_arrayValues.php
+++
@@ -1,9 +0,0 @@
-<?php
-$test = array(1,2,3,4);
-print_r($test);
-echo $test;
-foreach(array_values($test) as $item){
-	echo $item;
-}
-?>
-
